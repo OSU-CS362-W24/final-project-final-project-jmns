ad5d9c9e049c8c74690b3a9d5f51bf298d006cc6
Justin Pham <phamjus@oregonstate.edu>
2024-03-09 21:22:58 -0800

    Finished tests for clearing chart data

--- src/__tests__/ui_integration.test.js
+++ src/__tests__/ui_integration.test.js
@@ -8,6 +8,11 @@
     By Justin Pham
 */
 
+// Create a window.alert for the js to call to
+// Line referenced from: https://stackoverflow.com/a/55909840 by JRJurman
+window.alert = jest.fn()
+
+// Used to reset the local storage for each individual test
 const chartStorage = require(`${__dirname}/../lib/chartStorage`)
 
 const fs = require("fs")
@@ -39,11 +44,20 @@ function resetInputValues(){
 }
 
 beforeEach(() => {
+
+    // Clears the alert window of any alerts/message it gets
+    // Line referenced from: https://stackoverflow.com/a/55909840 by JRJurman
+    window.alert.mockClear()        
+
     resetInputValues()
 
     /*
         You can toggle which page in the website to test with
         Although each page (line, bar, scatter) is functionally the same
+
+        WHEN TESTING THIS PAGE USING THE BAR FILES, BE AWARE THAT THE
+        X VALUE INPUT HAS TYPE TEXT RATHER THAN TYPE NUMBER WHICH CAUSES
+        TESTS 2 and 3 TO BE BROKEN. 
     */
 
     initDomFromFiles(
@@ -218,12 +232,14 @@ test("Multiple input pairs can be added", async function () {
 *
 **********************************************/
 
-// No Values in Values section ------------------------------
+// No Values in Values and Label section --------------------
 
-// Error message pops up when missing XY values
-test("Error message pops up when missing XY values", async function () {
+// Error message pops up when missing values
+test("Error message pops up when missing values Error: No data specified!", async function () {
 
     const user = userEvent.setup()
+
+    const errorMessage = "Error: No data specified!"
 
     // Get Generate Chart Button
     const generateChartButton = domTesting.getByRole(document, "button", {name : "Generate chart"})
@@ -236,15 +252,27 @@ test("Error message pops up when missing XY values", async function () {
     // Assert that a message/alert appeared on the screen to be read
     expect(alert).toHaveBeenCalledTimes(1)
 
+    const alertMessage = alert.mock.lastCall[0]
+    expect(alertMessage).toEqual(errorMessage)
+
     alert.mockRestore()
 })
 
-// Error message displays the message Error: No data specified!
-test("Error message displays the message Error: No data specified!", async function () {
+// No Values in Values section ------------------------------
+
+// Error message displays the message Error: No data specified! when missing XY values
+test("Error message displays the message Error: No data specified! when missing XY values", async function () {
 
     const user = userEvent.setup()
 
     const errorMessage = "Error: No data specified!"
+
+    // Fill in values for label section
+    const xLabel = domTesting.getByLabelText(document, "X label")
+    await user.type(xLabel, "dummy")
+
+    const yLabel = domTesting.getByLabelText(document, "Y label")
+    await user.type(yLabel, "dummy")
 
     // Get Generate Chart Button
     const generateChartButton = domTesting.getByRole(document, "button", {name : "Generate chart"})
@@ -264,15 +292,110 @@ test("Error message displays the message Error: No data specified!", async funct
     alert.mockRestore()
 })
 
+
 // No Labels in Labels section ------------------------------
 
 // Missing both labels pops up an error message
+test("Missing both labels pops up an error message Error: Must specify a label for both X and Y!", async function () {
 
-// Missing x label pops up an error message ? 
+    const user = userEvent.setup()
 
-// Missing y label pops up an error message ?
+    const errorMessage = "Error: Must specify a label for both X and Y!"
 
-// Error message displays the message Error: Must specify a label for both X and Y!
+    // Fill in values for values section
+    const dummy = '5'
+    const xInput = domTesting.getByLabelText(document, "X")
+    await user.type(xInput, dummy)
+
+    const yInput = domTesting.getByLabelText(document, "Y")
+    await user.type(yInput, dummy)
+
+    // Get Generate Chart Button
+    const generateChartButton = domTesting.getByRole(document, "button", {name : "Generate chart"})
+
+    // Create a spy for the alert
+    const alert = jest.spyOn(window, "alert")
+
+    await user.click(generateChartButton)
+
+    // Assert that a message/alert appeared on the screen to be read
+    expect(alert).toHaveBeenCalledTimes(1)
+
+    const alertMessage = alert.mock.lastCall[0]
+    expect(alertMessage).toEqual(errorMessage)
+
+    alert.mockRestore()
+})
+
+// Missing x label pops up an error message
+test("Missing x label pops up an error message Error: Must specify a label for both X and Y!", async function () {
+
+    const user = userEvent.setup()
+
+    const errorMessage = "Error: Must specify a label for both X and Y!"
+
+    // Fill in values for values section
+    const dummy = '5'
+    const xInput = domTesting.getByLabelText(document, "X")
+    await user.type(xInput, dummy)
+
+    const yInput = domTesting.getByLabelText(document, "Y")
+    await user.type(yInput, dummy)
+
+    const yLabel = domTesting.getByLabelText(document, "Y label")
+    await user.type(yLabel, "dummy")
+
+    // Get Generate Chart Button
+    const generateChartButton = domTesting.getByRole(document, "button", {name : "Generate chart"})
+
+    // Create a spy for the alert
+    const alert = jest.spyOn(window, "alert")
+
+    await user.click(generateChartButton)
+
+    // Assert that a message/alert appeared on the screen to be read
+    expect(alert).toHaveBeenCalledTimes(1)
+
+    const alertMessage = alert.mock.lastCall[0]
+    expect(alertMessage).toEqual(errorMessage)
+
+    alert.mockRestore()
+})
+
+// Missing y label pops up an error message
+test("Missing y label pops up an error message Error: Must specify a label for both X and Y!", async function () {
+
+    const user = userEvent.setup()
+
+    const errorMessage = "Error: Must specify a label for both X and Y!"
+
+    // Fill in values for values section
+    const dummy = '5'
+    const xInput = domTesting.getByLabelText(document, "X")
+    await user.type(xInput, dummy)
+
+    const yInput = domTesting.getByLabelText(document, "Y")
+    await user.type(yInput, dummy)
+
+    const xLabel = domTesting.getByLabelText(document, "X label")
+    await user.type(xLabel, "dummy")
+
+    // Get Generate Chart Button
+    const generateChartButton = domTesting.getByRole(document, "button", {name : "Generate chart"})
+
+    // Create a spy for the alert
+    const alert = jest.spyOn(window, "alert")
+
+    await user.click(generateChartButton)
+
+    // Assert that a message/alert appeared on the screen to be read
+    expect(alert).toHaveBeenCalledTimes(1)
+
+    const alertMessage = alert.mock.lastCall[0]
+    expect(alertMessage).toEqual(errorMessage)
+
+    alert.mockRestore()
+})
 
 /**********************************************
 *
@@ -283,14 +406,261 @@ test("Error message displays the message Error: No data specified!", async funct
 **********************************************/
 
 // Clearing chart data reverts Chart Color to default color
+test("Clearing chart data reverts Chart Color to default color", async function() {
+
+    const user = userEvent.setup()
+
+    /*
+        Fill in dummy values for every input
+    */
+    const chartTitle = domTesting.getByLabelText(document, "Chart title")
+    await user.type(chartTitle, "dummy")
+    
+    const xLabel = domTesting.getByLabelText(document, "X label")
+    await user.type(xLabel, "dummy")
+    
+    const yLabel = domTesting.getByLabelText(document, "Y label")
+    await user.type(yLabel, "dummy")
+    
+    const chartColor = domTesting.getByLabelText(document, "Chart color")
+    
+    // Line referenced from: https://github.com/testing-library/user-event/issues/423
+    // No easy way to simulate picking a color from what I can find
+    domTesting.fireEvent.input(chartColor, {target: {value: '#ff00ff'}})
+    
+    const plusButton = domTesting.getByRole(document, "button", {name : "+"})
+    
+    await user.click(plusButton)
+    await user.click(plusButton)
+    await user.click(plusButton)
+    
+    const dataXInputs = domTesting.queryAllByLabelText(document, "X")
+    const dataYInputs = domTesting.queryAllByLabelText(document, "Y")
+    
+    const dummy = '5'
+    await user.type(dataXInputs[0], dummy)
+    await user.type(dataYInputs[0], dummy)
+    await user.type(dataXInputs[1], dummy)
+    await user.type(dataYInputs[1], dummy)
+    await user.type(dataXInputs[2], dummy)
+    await user.type(dataYInputs[2], dummy)
+
+    // Get the clear chart button
+    const clearChartData = domTesting.getByRole(document, "button", {name : "Clear chart data"})
+
+    await user.click(clearChartData)
+
+    // Assert that the Color is back to the default color #ff4500
+    expect(chartColor).toHaveValue('#ff4500')
+})
 
 // Clearing chart data clears Chart Title
+test("Clearing chart data clears Chart Title", async function() {
+
+    const user = userEvent.setup()
+
+    /*
+        Fill in dummy values for every input
+    */
+    const chartTitle = domTesting.getByLabelText(document, "Chart title")
+    await user.type(chartTitle, "dummy")
+    
+    const xLabel = domTesting.getByLabelText(document, "X label")
+    await user.type(xLabel, "dummy")
+    
+    const yLabel = domTesting.getByLabelText(document, "Y label")
+    await user.type(yLabel, "dummy")
+    
+    const chartColor = domTesting.getByLabelText(document, "Chart color")
+    
+    // Line referenced from: https://github.com/testing-library/user-event/issues/423
+    // No easy way to simulate picking a color from what I can find
+    domTesting.fireEvent.input(chartColor, {target: {value: '#ff00ff'}})
+    
+    const plusButton = domTesting.getByRole(document, "button", {name : "+"})
+    
+    await user.click(plusButton)
+    await user.click(plusButton)
+    await user.click(plusButton)
+    
+    const dataXInputs = domTesting.queryAllByLabelText(document, "X")
+    const dataYInputs = domTesting.queryAllByLabelText(document, "Y")
+    
+    const dummy = '5'
+    await user.type(dataXInputs[0], dummy)
+    await user.type(dataYInputs[0], dummy)
+    await user.type(dataXInputs[1], dummy)
+    await user.type(dataYInputs[1], dummy)
+    await user.type(dataXInputs[2], dummy)
+    await user.type(dataYInputs[2], dummy)
+
+    // Get the clear chart button
+    const clearChartData = domTesting.getByRole(document, "button", {name : "Clear chart data"})
+
+    await user.click(clearChartData)
+
+    // Assert that the title is empty
+    expect(chartTitle).not.toHaveValue()
+})
 
 // Clearing chart data clears Data Labels
+test("Clearing chart data clears Data Labels", async function() {
+
+    const user = userEvent.setup()
+
+    /*
+        Fill in dummy values for every input
+    */
+    const chartTitle = domTesting.getByLabelText(document, "Chart title")
+    await user.type(chartTitle, "dummy")
+    
+    const xLabel = domTesting.getByLabelText(document, "X label")
+    await user.type(xLabel, "dummy")
+    
+    const yLabel = domTesting.getByLabelText(document, "Y label")
+    await user.type(yLabel, "dummy")
+    
+    const chartColor = domTesting.getByLabelText(document, "Chart color")
+    
+    // Line referenced from: https://github.com/testing-library/user-event/issues/423
+    // No easy way to simulate picking a color from what I can find
+    domTesting.fireEvent.input(chartColor, {target: {value: '#ff00ff'}})
+    
+    const plusButton = domTesting.getByRole(document, "button", {name : "+"})
+    
+    await user.click(plusButton)
+    await user.click(plusButton)
+    await user.click(plusButton)
+    
+    const dataXInputs = domTesting.queryAllByLabelText(document, "X")
+    const dataYInputs = domTesting.queryAllByLabelText(document, "Y")
+    
+    const dummy = '5'
+    await user.type(dataXInputs[0], dummy)
+    await user.type(dataYInputs[0], dummy)
+    await user.type(dataXInputs[1], dummy)
+    await user.type(dataYInputs[1], dummy)
+    await user.type(dataXInputs[2], dummy)
+    await user.type(dataYInputs[2], dummy)
+
+    // Get the clear chart button
+    const clearChartData = domTesting.getByRole(document, "button", {name : "Clear chart data"})
+
+    await user.click(clearChartData)
+
+    // Assert that the data labels are empty
+    expect(xLabel).not.toHaveValue()
+    expect(yLabel).not.toHaveValue()
+})
 
 // Clearing chart data reverts it to having only a pair of inputs for values
+test("Clearing chart data reverts it to having only a pair of inputs for values", async function() {
+
+    const user = userEvent.setup()
+
+    /*
+        Fill in dummy values for every input
+    */
+    const chartTitle = domTesting.getByLabelText(document, "Chart title")
+    await user.type(chartTitle, "dummy")
+    
+    const xLabel = domTesting.getByLabelText(document, "X label")
+    await user.type(xLabel, "dummy")
+    
+    const yLabel = domTesting.getByLabelText(document, "Y label")
+    await user.type(yLabel, "dummy")
+    
+    const chartColor = domTesting.getByLabelText(document, "Chart color")
+    
+    // Line referenced from: https://github.com/testing-library/user-event/issues/423
+    // No easy way to simulate picking a color from what I can find
+    domTesting.fireEvent.input(chartColor, {target: {value: '#ff00ff'}})
+    
+    const plusButton = domTesting.getByRole(document, "button", {name : "+"})
+    
+    await user.click(plusButton)
+    await user.click(plusButton)
+    await user.click(plusButton)
+    
+    var dataXInputs = domTesting.queryAllByLabelText(document, "X")
+    var dataYInputs = domTesting.queryAllByLabelText(document, "Y")
+    
+    const dummy = '5'
+    await user.type(dataXInputs[0], dummy)
+    await user.type(dataYInputs[0], dummy)
+    await user.type(dataXInputs[1], dummy)
+    await user.type(dataYInputs[1], dummy)
+    await user.type(dataXInputs[2], dummy)
+    await user.type(dataYInputs[2], dummy)
+
+    // Get the clear chart button
+    const clearChartData = domTesting.getByRole(document, "button", {name : "Clear chart data"})
+
+    await user.click(clearChartData)
+
+    // Assert that there are only one pair of inputs for values
+    dataXInputs = domTesting.queryAllByLabelText(document, "X")
+    dataYInputs = domTesting.queryAllByLabelText(document, "Y")
+
+    expect(dataXInputs).toHaveLength(1)
+    expect(dataYInputs).toHaveLength(1)
+})
 
 // Clearing chart data clears data from values
+test("Clearing chart data clears data from values", async function() {
+
+    const user = userEvent.setup()
+
+    /*
+        Fill in dummy values for every input
+    */
+    const chartTitle = domTesting.getByLabelText(document, "Chart title")
+    await user.type(chartTitle, "dummy")
+    
+    const xLabel = domTesting.getByLabelText(document, "X label")
+    await user.type(xLabel, "dummy")
+    
+    const yLabel = domTesting.getByLabelText(document, "Y label")
+    await user.type(yLabel, "dummy")
+    
+    const chartColor = domTesting.getByLabelText(document, "Chart color")
+    
+    // Line referenced from: https://github.com/testing-library/user-event/issues/423
+    // No easy way to simulate picking a color from what I can find
+    domTesting.fireEvent.input(chartColor, {target: {value: '#ff00ff'}})
+    
+    const plusButton = domTesting.getByRole(document, "button", {name : "+"})
+    
+    await user.click(plusButton)
+    await user.click(plusButton)
+    await user.click(plusButton)
+    
+    var dataXInputs = domTesting.queryAllByLabelText(document, "X")
+    var dataYInputs = domTesting.queryAllByLabelText(document, "Y")
+    
+    const dummy = '5'
+    await user.type(dataXInputs[0], dummy)
+    await user.type(dataYInputs[0], dummy)
+    await user.type(dataXInputs[1], dummy)
+    await user.type(dataYInputs[1], dummy)
+    await user.type(dataXInputs[2], dummy)
+    await user.type(dataYInputs[2], dummy)
+
+    // Get the clear chart button
+    const clearChartData = domTesting.getByRole(document, "button", {name : "Clear chart data"})
+
+    await user.click(clearChartData)
+
+    // Assert that there are only one pair of inputs for values
+    dataXInputs = domTesting.queryAllByLabelText(document, "X")
+    dataYInputs = domTesting.queryAllByLabelText(document, "Y")
+
+    expect(dataXInputs).toHaveLength(1)
+    expect(dataYInputs).toHaveLength(1)
+
+    expect(dataXInputs[0]).not.toHaveValue()
+    expect(dataYInputs[0]).not.toHaveValue()
+})
 
 /**********************************************
 *
