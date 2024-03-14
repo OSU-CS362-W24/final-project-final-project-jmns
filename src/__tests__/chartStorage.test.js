0f28a23a251bce504b170578ccd1c3196de8444f
mfaks <muhammad.faks@gmail.com>
2024-03-14 14:24:21 -0700

    feat: added tests for loadAllSavedCharts function from chartStorage.js

--- src/__tests__/chartStorage.test.js
+++ src/__tests__/chartStorage.test.js
@@ -73,8 +73,8 @@ describe("Testing for saveChart function begins here", () => {
         const savedCharts = JSON.parse(window.localStorage.getItem('savedCharts'));
     
         //ASSERT
-        expect(savedCharts).not.toBeNull();
-        expect(savedCharts.length).toBe(2);
+        expect(savedCharts).not.toBeNull()
+        expect(savedCharts.length).toBe(2)
         expect(savedCharts[0]).toEqual(initialChart)
         expect(savedCharts[1]).toEqual(SecondaryChart)
     });
@@ -83,8 +83,57 @@ describe("Testing for saveChart function begins here", () => {
 //Tests for loadAllSavedCharts function start here - - - 
 describe("Testing for loadAllSavedCharts function begins here", () => {
 
-})
+    beforeEach(() => {
+        window.localStorage.clear()
+    })
 
+    test("loadAllSavedCharts function returns an empty array when localStorage is empty", () => {
+        
+        //ACT
+        const charts = loadAllSavedCharts()
+        
+        //ASSERT
+        expect(charts).toEqual([])
+        expect(charts.length).toBe(0)
+        
+    })
+
+    test("loadAllSavedCharts function can return a single chart from local storage", () => {
+
+        //ARRANGE
+        const firstChart = { title: 'First Chart', data: [{'x': 1, 'y': 1}, {'x': 2, 'y': 2}, {'x': 3, 'y': 3}] };
+        
+        //ACT
+        saveChart(firstChart)
+        const loadedCharts = loadAllSavedCharts();
+
+        //ASSERT
+        expect(loadedCharts).not.toBeNull()
+        expect(loadedCharts.length).toBe(1)
+        expect(loadedCharts[0]).toEqual(firstChart);
+
+    })
+
+    test("loadAllSavedCharts function can return multiple charts from local storage", () => {
+
+        //ARRANGE
+        const initialChart = { title: 'Intitial Chart', data: [{'x': 1, 'y': 1}, {'x': 2, 'y': 2}, {'x': 3, 'y': 3}] };
+        const secondaryChart = { title: 'Secondary Chart', data: [{'x': 4, 'y': 4}, {'x': 5, 'y': 5}, {'x': 6, 'y': 6}] };
+        
+        //ACT
+        saveChart(initialChart)
+        saveChart(secondaryChart)
+        const loadedCharts = loadAllSavedCharts();
+
+        //ASSERT
+        expect(loadedCharts).not.toBeNull()
+        expect(loadedCharts.length).toBe(2)
+        expect(loadedCharts[0]).toEqual(initialChart);
+        expect(loadedCharts[1]).toEqual(secondaryChart);
+
+    })
+
+})
 
 
 //Tests for loadSavedCharts function start here - - - 
